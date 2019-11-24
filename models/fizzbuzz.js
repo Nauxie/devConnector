633ff1d7cbc033a32955db0eed7a71a34f08ee4c
Nauxie <abhinav.tumu1@gmail.com>
2019-11-24 15:15:52 -0800

    profile login api

--- models/fizzbuzz.js
+++ models/fizzbuzz.js
@@ -6,7 +6,6 @@ function a() {
   let x = 2
   b(x)
 }
-
 let x = 1
 a()
 console.log(x)
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
 const User = require('../../models/User')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
 
 //@route  GET api/auth
 //desc    test route
@@ -16,5 +20,60 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server error')
   }
 })
+//@route  POST api/auth
+//desc    Authenticate user & get token
+//@access Public
+
+router.post(
+  '/',
+  [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a valid password').exists()
+  ],
+  async (req, res) => {
+    //console.log(req.body)
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { email, password } = req.body
+
+    try {
+      let user = await User.findOne({ email })
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] })
+      }
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        {
+          expiresIn: 36000
+        },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
+    } catch (err) {
+      console.error(err.mesage)
+      res.status(500).send('Server error')
+    }
+  }
+)
 
 module.exports = router
